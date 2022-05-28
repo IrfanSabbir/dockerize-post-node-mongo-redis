cfe4a9a105afafd211660fc6a1d906e9ee8d01ed
IrfanSabbir <avoidirfan@gmail.com>
2022-05-28 12:47:14 +0600

    Docker command file updated with several commands to follow

--- docker-command.js
+++ docker-command.js
@@ -22,4 +22,46 @@
  * -------------------------------------
  * 
  *  ==> docker volume ls
+ * 
+ * 
+ *  docker logs
+ * -------------------------------------
+ * 
+ *  ==> docker logs container name(node-app)
+ * 
+ * Pass other volume (bind mount) to sync between docker container and local changes(dev)
+ * -------------------------------------
+ * 
+ *  ==> docker run -v %cd%:/app -p 3000:3000 -d --name node-app node-app-image
+ *  ==> docker run -v {pwd}:/app -p 3000:3000 -d --name node-app node-app-image
+ * 
+ * 
+ * 
+ *  Pass other volume (anonymous) to not to touch node_module from container(dev)
+ * -------------------------------------
+ * 
+ *  ==> docker run -v %cd%:/app -v /app/node_modules -p 3000:3000 -d --name node-app node-app-image
+ *  ==> docker run -v {pwd}:/app -v /app/node_modules -p 3000:3000 -d --name node-app node-app-image
+ * 
+ * 
+ * 
+ *  Run docker with read only mode(dev)
+ * -------------------------------------
+ * 
+ *  ==> docker run -v %cd%:/app:ro -v /app/node_modules -p 3000:3000 -d --name node-app node-app-image
+ *  ==> docker run -v {pwd}:/app:ro -v /app/node_modules -p 3000:3000 -d --name node-app node-app-image
+ * 
+ * 
+ * Run docker with env from terminal
+ * -------------------------------------
+ * 
+ *  ==> docker run -v %cd%:/app:ro -v /app/node_modules --env PORT:3000 -p 3000:3000 -d --name node-app node-app-image
+ *  ==> docker run -v {pwd}:/app:ro -v /app/node_modules --env PORT:3000 -p 3000:3000 -d --name node-app node-app-image
+ * 
+ *  * Run docker with env_file from terminal
+ * -------------------------------------
+ * 
+ *  ==> docker run -v %cd%:/app:ro -v /app/node_modules --env-file ./.env-p 3000:3000 -d --name node-app node-app-image
+ *  ==> docker run -v {pwd}:/app:ro -v /app/node_modules --env-file ./.env -p 3000:3000 -d --name node-app node-app-image
+ * 
  */
